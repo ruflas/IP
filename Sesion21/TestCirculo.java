fcabb8776177754fb238bf7e3784b9a0c019d1e2
juan75555 <72112435+juan75555@users.noreply.github.com>
2021-11-24 10:29:29 +0100

    Update and rename TestPuntoJPanel to TestCirculo.java

--- Sesion21/TestCirculo.java
+++ Sesion21/TestCirculo.java
@@ -1,16 +1,15 @@
-//Tenia que haber sido con la clase circulo pero añadi los metodos getRadio y sirve igual
 import javax.swing.JFrame;
-public class TestPunto {
+public class TestCirculo {
 
 	public static void main(String[] args) {
 		JFrame app = new JFrame("Pintando circulo");
 		app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		app.setBounds(0, 0, 300, 200);
-		Punto P = new Punto();
-		P.setX(40);
-		P.setY(25);
-		P.setRadio(200);
-		app.add(P);
+		Circulo C = new Circulo();
+		C.setX(40);
+		C.setY(25);
+		C.setRadio(200);
+		app.add(C);
 		app.setVisible(true);
 	}
 
